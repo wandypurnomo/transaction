07dd44e28dccf6c409cef0113ddf158c8bf7ba6d
wandy purnomo <wandypurnomo92@gmail.com>
2020-02-02 15:40:51 +0700

    improve helper

--- src/Helpers/transaction_helper.php
+++ src/Helpers/transaction_helper.php
@@ -1,12 +1,12 @@
 <?php
 
 if (!function_exists("getCurrentCart")) {
-    function getCurrentCart(string $userId): ?\Illuminate\Database\Eloquent\Model
+    function getCurrentCart(string $userId): ?\Wandxx\Transaction\Services\Cart\CartService
     {
         $cartService = resolve(\Wandxx\Transaction\Services\Cart\CartService::class);
 
-        if ($model = $cartService->getCurrentCart($userId) instanceof \Illuminate\Database\Eloquent\Model) {
-            return $cartService->getCurrentCart($userId);
+        if ($model = $cartService instanceof \Wandxx\Transaction\Services\Cart\CartService) {
+            return $cartService;
         }
 
         return null;
